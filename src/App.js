21c30c332365e54427d8bca397c529e690d3da00
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2023-09-11 15:38:08 +0530

    style: fixed carousel

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Hero />
       <AboutCard />
-      {/* <SimpleSlider /> */}
+      <SimpleSlider />
       <Footer />
     </div>
   );
--- src/Component/Article/Article.js
+++ src/Component/Article/Article.js
@@ -10,25 +10,67 @@ import Slider from "react-slick";
 export const SimpleSlider = () => {
   const settings = {
     dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    className: "slider",
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <section className="article_section">
       <h2>Latest Articles </h2>
-      <Slider {...settings} className="slider">
-        {articleData.map((item) => (
-          <li key={item.id} className="article_card">
-            <img src={item.img} alt="article-hero-img" />
-            <h3>{item.title}</h3>
-            <p>{item.content}</p>
-            <button>Read More</button>
-          </li>
-        ))}
-      </Slider>
+      <div className="slider">
+        <Slider {...settings}>
+          {articleData.map((item) => (
+            <div
+              key={item.id}
+              className="article_card"
+              style={{ display: "flex" }}
+            >
+              <div className="card_img_container">
+                <img src={item.img} alt="article-hero-img" />
+              </div>
+              <h3>{item.title}</h3>
+              <p>{item.content}</p>
+              <button>Read More</button>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </section>
   );
 };
